a0ef1394668988f8b1d14257ba6b4ce8885f4fc4
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2021-02-11 11:31:36 +0100

    updated dependencies

--- esm/index.js
+++ esm/index.js
@@ -266,7 +266,7 @@ defineProperty(customElements, 'define', {
     whenDefined(is).then(() => {
       if (tag) {
         parse(document.querySelectorAll(selector));
-        shadows.forEach(parseShadow, [selector]);
+        shadows.forEach(parseShadow, selector);
       }
       else
         parseShadowed(document.querySelectorAll(selector));
@@ -276,7 +276,7 @@ defineProperty(customElements, 'define', {
 });
 function parseShadow(element) {
   const {parse, root} = shadowRoots.get(element);
-  parse(root.querySelectorAll(this), element.isConnected);
+  parse(root.querySelectorAll('' + this), element.isConnected);
 }
   }
 };
